e73636e001fd71f485c31a6cc490e01e478cfa3e
phucnguyenbss <phuc.nguyen@beesightsoft.com>
2020-07-13 11:15:17 +0700

    update code

--- app/Http/Controllers/Api/PhoneController.php
+++ app/Http/Controllers/Api/PhoneController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Helper\GoogleApiIdentityClient;
 use App\Verification;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Psr7;
 
 class PhoneController extends Controller
 {
@@ -26,8 +28,19 @@ class PhoneController extends Controller
                 'session_info' => $body["sessionInfo"],
             ]);
             return $verification;
-        } catch (\Exception $e) {
-            return $e->getMessage();
+        } catch (ClientException $e) {
+            if ($e->getResponse()->getStatusCode() == 400 && array_key_exists("error", json_decode($e->getResponse()->getBody(),true))) {
+                switch (json_decode($e->getResponse()->getBody(),true)["error"]["message"]) {
+                    case "INVALID_PHONE_NUMBER : Invalid format.":
+                        return "INVALID_PHONE_NUMBER : Invalid format.";
+                        break;  
+                    default:
+                        return json_decode($e->getResponse()->getBody(),true);
+                  }
+            }
+        }
+        catch (\Exception $e) {
+            return "error: " .$e->getMessage();
         }
     }
 
@@ -38,15 +51,28 @@ class PhoneController extends Controller
             $verification = Verification::where('phone', $data["phoneNumber"])->first();
             if ($verification) {
                 $response = $this->client->verifyPhoneNumber($data["code"], $verification["session_info"]);
-                $body = $response->getBody();
+                $body = json_decode($response->getBody(),true);
                 $verification->verify = true;
                 $verification->save();
                 return $body;
             }
-
             return "Not Found";
-        } catch (\Exception $e) {
-            return $e->getMessage();
+        } catch (ClientException $e) {
+            if ($e->getResponse()->getStatusCode() == 400 && array_key_exists("error", json_decode($e->getResponse()->getBody(),true))) {
+                switch (json_decode($e->getResponse()->getBody(),true)["error"]["message"]) {
+                    case "SESSION_EXPIRED":
+                        return "expired";
+                        break;  
+                    case "INVALID_CODE":
+                        return "INVALID_CODE";
+                        break;  
+                    default:
+                        return json_decode($e->getResponse()->getBody(),true);
+                  }
+            }
+        }
+        catch (\Exception $e) {
+            return "error: " .$e->getMessage();
         }
     }
 }
